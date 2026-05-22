1c4a0f29bfe0f11d9857810e5ac0d064792299d9
jmorallos <joevermorallos2001@gmail.com>
2026-05-22 16:38:24 +0800

    add character classes

---
+++ 04regex_character_classes.js
@@ -0,0 +1,4 @@
+// The wildcard class represented by dot .
+const regex = /[a-zA-Z0-9]/; // a - z A - Z 0 - 9 pattern if u want literal hyphen u can add it in the beginning -a-z
+const regex2 = /[-\w]/; 
+
